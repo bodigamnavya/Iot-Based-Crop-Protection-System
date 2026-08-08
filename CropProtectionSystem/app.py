33009e03195066b475f7c6bdcc23405d93f96725
bodigamnavya <bodigamnavya@gmail.com>
2026-08-08 20:17:27 +0530

    Remove playsound for Render

--- CropProtectionSystem/app.py
+++ CropProtectionSystem/app.py
@@ -6,11 +6,11 @@ from alarm import play_alarm
 from water_sensor import get_soil_moisture
 from datetime import datetime
 
-app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'crop_protection'
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT', 3306))
 
 mysql = MySQL(app)
 # Load YOLO model
